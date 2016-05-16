1ac0e14506f21944f9f3e5a80cd4914db61ea68f
Steve Louie <stephen.r.louie@gmail.com>
2016-05-16 05:03:37 +0000

    crontab script for virtualenv

---
+++ cron.sh
@@ -0,0 +1,13 @@
+#! /bin/bash    
+cd /home/stelouie/uber
+
+source bin/activate
+
+# virtualenv is now active, which means your PATH has been modified.
+# Don't try to run python from /usr/bin/python, just run "python" and
+# let the PATH figure out which version to run (based on what your
+# virtualenv has configured).
+
+cd src
+
+python price.py
