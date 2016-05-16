2c9e476910ef02bd247fde945323b14880c41ddc
Steve Louie <stephen.r.louie@gmail.com>
2016-05-16 04:38:14 +0000

    Added boston locations. DB functionality added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Data collection for Uber surge pricing 
+#Data collection for Uber surge pricing 
+
+First boston set is from the top ten places in boston for Uber, found [here](https://www.boston.com/culture/food/2016/03/07/these-are-the-ten-most-popular-nightlife-spots-in-boston-according-to-uber)
--- src/price.py
+++ src/price.py
@@ -2,34 +2,40 @@ import json
 import os 
 import time
 import configparser
+import pymongo
 from uber_rides.client import UberRidesClient
 from uber_rides.session import Session
 
 CONFIG_NAME = 'config.ini'
 LOCATION_NAME = 'locations.ini'
-CITIES_LIST = ['BOSTON', 'SAN_FRAN']
+CITIES_LIST = ['BOSTON']
 
 def get_prices(start_lat, start_lng, end_lat, end_lng):
-    response = client.get_price_estimates(start_lat, start_lng, end_lat, end_lng)
     cars = []
     surges = []
-    
+    #try:
+    response = client.get_price_estimates(start_lat, start_lng, end_lat, end_lng)
     # assert response looks like price estimates
     jresponse = response.json
     for prod in jresponse['prices']:
         cars.append(prod['localized_display_name'])
         surges.append(prod['surge_multiplier'])
     return cars, surges
+    #except:
+    #    return cars,surges
+
+def listify_config(string):
+    return string.replace(' ','').split(",")
 
 
 def load_location_data(city_name):
     location_config = configparser.ConfigParser()
     location_config.read(LOCATION_NAME)
     names = location_config.get(city_name, 'names').split(",")
-    start_lats = location_config.get(city_name, 'start_lats').split(",")
-    start_lngs = location_config.get(city_name, 'start_lngs').split(",")
-    end_lats = location_config.get(city_name, 'end_lats').split(",")
-    end_lngs = location_config.get(city_name, 'end_lngs').split(",")
+    start_lats = listify_config(location_config.get(city_name, 'start_lats'))
+    start_lngs = listify_config(location_config.get(city_name, 'start_lngs'))
+    end_lats = listify_config(location_config.get(city_name, 'end_lats'))
+    end_lngs = listify_config(location_config.get(city_name, 'end_lngs'))
     time_zone = location_config.get(city_name, 'time_zone')
     return names, start_lats, start_lngs, end_lats, end_lngs, time_zone
 
@@ -38,6 +44,25 @@ def get_local_time(time_zone):
     os.environ['TZ'] = time_zone
     time.tzset()
     return time.strftime("%d-%m-%y %H:%M")
+
+
+def connect_to_mongo():
+    try:
+        return pymongo.MongoClient()
+    except:
+        print("Could not connect to mongo")
+
+def connect_to_db(mongo_client, name):
+    try:
+        return mongo_client[name]
+    except: print("Could not connect to db name")
+
+def disconnect_from_mongo(mongo_client):
+    mongo_client.close()
+
+
+mongo_client = connect_to_mongo()
+mongo = connect_to_db(mongo_client, "uber")
 
 config = configparser.ConfigParser()
 config.read(CONFIG_NAME)
@@ -48,13 +73,10 @@ client = UberRidesClient(session)
 
 for city in CITIES_LIST:
     names, start_lats, start_lngs, end_lats, end_lngs, time_zone = load_location_data(city)
-    localt = get_local_time(time_zone) 
-    print "------"
-    print city
-    print localt
+    localtime = get_local_time(time_zone) 
     for index in range(len(names)):
-        print "-----"
-        print names[index] 
         cars, surges = get_prices(start_lats[index], start_lngs[index], end_lats[index], end_lngs[index])
-        print cars
-        print surges
+        entry = {'city': city, 'name': names[index], 'time': localtime, 'cars': cars, 'surge': surges} 
+        mongo.surge.insert_one(entry)
+
+disconnect_from_mongo(mongo_client)
